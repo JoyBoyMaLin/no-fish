fb5380fa2d0a37a21e6372e422e2c6b801e7bf86
malin <malinsuse@gmail.com>
2021-09-15 00:36:13 +0800

    add: captcha and login error rate limit for login

--- fish/account/views.py
+++ fish/account/views.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from ratelimit.decorators import ratelimit
 
 
-@ratelimit(key='ip', rate='2/h', block=True)
+@ratelimit(key='ip', rate='5/h', block=True)
 def extend_admin_login(request, extra_context=None):
     return admin.site.login(request, extra_context)
